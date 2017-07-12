09e4d7bda36bf9006cdbda687d48f2a8ed33eff6
alexxxmf <amoro@alumni.unav.es>
2017-07-12 23:24:32 +0100

    Still playing with stuff

--- 01/index.js
+++ 01/index.js
@@ -7,6 +7,9 @@ window.addEventListener('keypress', function(event){
 	*/
 	if (event.keyCode == 97){
 		console.log(event);
+		// We can selec the element given a certain attribute
 		var element = document.querySelector('[data-key="97"]')
+		// Let's add a class with certain styling when event is triggered
+		element.classList.add('highlight')
 	}
 });
