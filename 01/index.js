66130774fbdbbc275457bc5d4eea8fdc522e0cb4
alexxxmf <amoro@alumni.unav.es>
2017-07-13 00:02:10 +0100

    Changed event

--- 01/index.js
+++ 01/index.js
@@ -1,15 +1,4 @@
-window.addEventListener('keypress', function(event){
-	/* 
-	KEYBOARD
+window.addEventListener('keydown', function(event){
+	console.log(event.keyCode)
 
-	97 is 'a'
-
-	*/
-	if (event.keyCode == 97){
-		console.log(event);
-		// We can selec the element given a certain attribute
-		var element = document.querySelector('[data-key="97"]')
-		// Let's add a class with certain styling when event is triggered
-		element.classList.add('highlight')
-	}
-});
+})
