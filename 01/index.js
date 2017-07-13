1969d97bffabd59cdf99683419f317bdb37b6c35
alexxxmf <amoro@alumni.unav.es>
2017-07-13 20:24:51 +0100

    Added some styling when pushing keys

--- 01/index.js
+++ 01/index.js
@@ -1,6 +1,12 @@
 window.addEventListener('keydown', function(event){
 	var keyElement = document.querySelector('.key[data-key=\"' + event.keyCode + '\"]');
-	var audioClipElement = document.querySelector('audio[data-key=\"' + event.keyCode + '\"]');
-	audioClipElement.play()
+	var audioElement = document.querySelector('audio[data-key=\"' + event.keyCode + '\"]');
+	// Shortcut for saying, if audio is empty (declared but still undefined)
+	if(!audioElement){
+		return;
+	}
+	audioElement.currentTime = 0
+	audioElement.play()
+	keyElement.classList.add('pushed')
 
 })
