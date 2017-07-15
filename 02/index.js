7cb848ab1ff26dd56fa099a7f1b2717acace79ee
alexxxmf <amoro@alumni.unav.es>
2017-07-15 11:50:26 +0100

    Added movement to all the three hands

--- 02/index.js
+++ 02/index.js
@@ -12,10 +12,22 @@ https://stackoverflow.com/questions/729921/settimeout-or-setinterval
 function setDate(){
 	var date = new Date
 	console.log(date)
+
 	var second = date.getSeconds()
+	var secondDegrees = ((second/60) * 360) + 90
+	var secondHand = document.querySelector('.secondHand')
+	secondHand.style.transform = "rotate(" + secondDegrees + "deg)"
+
 	var minute = date.getMinutes()
+	var minuteDegrees = ((minute/60) * 360) + 90
+	var minuteHand = document.querySelector('.minuteHand')
+	minuteHand.style.transform = "rotate(" + minuteDegrees + "deg)"
+
 	var hour = date.getHours()
-	
+	var hourDegrees = ((hour/24) * 360) + 90
+	var hourHand = document.querySelector('.hourHand')
+	hourHand.style.transform = "rotate(" + hourDegrees + "deg)"
+
 }
 
 setInterval(setDate, 1000)
