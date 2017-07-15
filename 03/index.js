67b4fa6613eb67aad7d0300e6f5c43c327bb422d
alexxxmf <amoro@alumni.unav.es>
2017-07-15 13:36:24 +0100

    Still experimenting with some stuff

--- 03/index.js
+++ 03/index.js
@@ -1,5 +1,12 @@
 var inputs = document.querySelectorAll('.controls input')
 inputs.forEach(function(input){
-	input.addEventListener('change', function(event){console.log(event)})
+	input.addEventListener(
+		'change',
+		function(event){
+			console.log(input.value)
+			//if sizing property is not in sizing go for empty string
+			var suffix = input.dataset.sizing || ""
+			
+		})
 })
 
