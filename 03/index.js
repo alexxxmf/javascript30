96b6b38ae48da2ffd2bfb9c832e305db73299593
alexxxmf <amoro@alumni.unav.es>
2017-07-15 13:47:09 +0100

    Fixed some issues with variables and added some notes

--- 03/index.js
+++ 03/index.js
@@ -3,10 +3,11 @@ inputs.forEach(function(input){
 	input.addEventListener(
 		'change',
 		function(event){
-			console.log(input.value)
-			//if sizing property is not in sizing go for empty string
+			// if sizing property is not in sizing go for empty string
 			var suffix = input.dataset.sizing || ""
-			
+			// the object that calls the function is the one stored in this
+			// we are listening to change in input object to trigger this so this = input
+			document.documentElement.style.setProperty("--" + this.name, this.value + suffix)
 		})
 })
 
