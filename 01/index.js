38bcba58bde35cc6146a5437fd5e30ea99e0755f
alexxxmf <amoro@alumni.unav.es>
2017-08-16 22:18:38 +0100

    Implemented ES6 template strings

--- 01/index.js
+++ 01/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('keydown', function(event){
-	const keyElement = document.querySelector('.key[data-key=\"' + event.keyCode + '\"]');
-	const audioElement = document.querySelector('audio[data-key=\"' + event.keyCode + '\"]');
+	const keyElement = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+	const audioElement = document.querySelector(`audio[data-key="${event.keyCode}"]`);
 	// Shortcut for saying, if audio is empty (declared but still undefined)
 	if(!audioElement){
 		return;
@@ -12,7 +12,7 @@ window.addEventListener('keydown', function(event){
 })
 
 window.addEventListener('keyup', function(event){
-	const keyElement = document.querySelector('.key[data-key=\"' + event.keyCode + '\"]');
+	const keyElement = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 	keyElement.classList.remove('playing')
 
 })
